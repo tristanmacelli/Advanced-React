6b9c2f8e5aec0ab6dbcbe3cf75e1d193b520cc8d
Tristan Macelli <tristanmacelli@Tristans-MacBook-Air.local>
2021-12-22 16:42:32 -0800

    [ADD] initial keystone config && User schema

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,36 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/indent */
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { User } from './schemas/User';
+
+const databaseURL =
+    process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+    maxAge: 60 * 60 * 24 * 360, // How long should users stay signed in
+    secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+    server: {
+        cors: {
+            origin: [process.env.FRONTEND_URL],
+            credentials: true,
+        },
+    },
+    db: {
+        adapter: 'mongoose',
+        url: databaseURL,
+        // TODO: Add data seeding here
+    },
+    lists: createSchema({
+        // Schema items go in here
+        User,
+    }),
+    ui: {
+        // TODO: change this for roles
+        isAccessAllowed: () => true,
+    },
+    // Add session values here
+});
---
+++ sick-fits/backend/schemas/User.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/indent */
+import { list } from '@keystone-next/keystone/schema';
+import { text, password, relationship } from '@keystone-next/fields';
+
+export const User = list({
+    // access;
+    // ui;
+    fields: {
+        name: text({ isRequired: true }),
+        email: text({ isRequired: true, isUnique: true }),
+        password: password(),
+        // TODO: add roles, cart & orders
+    },
+});
